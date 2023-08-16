9fb0e3b36c3a5ad8ae0c506af64cfc6b2bc37dbe
Sahil <ss6611786@gmail.com>
2023-08-16 20:03:33 +0530

    i have added routing in react-native

--- App.js
+++ App.js
@@ -1,13 +1,19 @@
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import Home from './components/Home';
+import Movie from './components/Movie';
 
 export default function App() {
+  const Stack = createNativeStackNavigator();
   return (
-    <View
-    >
-      <Text>Open up App.js to start working on your app!</Text>
-
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName='Home'>
+        <Stack.Screen name='Home' component={Home} />
+        <Stack.Screen name='Movie' component={Movie} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
