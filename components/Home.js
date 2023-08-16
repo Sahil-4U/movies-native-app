ff32a62a32d0b6feeb2eea70a17b2b6eb4624340
Sahil <ss6611786@gmail.com>
2023-08-16 21:23:52 +0530

    i have designed till home page now movies is remaining

--- components/Home.js
+++ components/Home.js
@@ -1,12 +1,50 @@
-import { View, Text } from 'react-native'
+import { View, Text, TouchableOpacity, StyleSheet } from 'react-native'
 import React from 'react'
 
-const Home = () => {
+const Home = ({ navigation }) => {
     return (
-        <View>
-            <Text>Home</Text>
+        <View style={styles.container}>
+            <Text style={styles.heading}>Welcome to MoviesHub </Text>
+            <TouchableOpacity style={styles.btn} activeOpacity={0.5} onPress={() => navigation.navigate('Movie')} >
+                <Text style={styles.btnText}>Explore</Text>
+            </TouchableOpacity>
         </View>
     )
 }
 
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center'
+    },
+    heading: {
+        fontSize: 25,
+        backgroundColor: "#FFA500",
+        color: 'white',
+        paddingHorizontal: 35,
+        paddingVertical: 20,
+        marginBottom: 10,
+
+    },
+    btn: {
+        backgroundColor: '#3498db', // Button background color
+        borderRadius: 8, // Rounded corners
+        paddingVertical: 12, // Vertical padding
+        paddingHorizontal: 20, // Horizontal padding
+        shadowColor: '#000', // Shadow color
+        shadowOffset: {
+            width: 0,
+            height: 2,
+        },
+        shadowOpacity: 0.3, // Shadow opacity
+        shadowRadius: 3, // Shadow radius
+        elevation: 5, // Android shadow elevation
+    },
+    btnText: {
+        color: '#fff', // Text color
+        fontSize: 16, // Text size
+        textAlign: 'center', // Text alignment
+    },
+});
 export default Home
